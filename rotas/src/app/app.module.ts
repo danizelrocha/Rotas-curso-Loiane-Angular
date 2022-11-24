188ecfa42bef333e9baa5e82155e09aefe2fc7c9
DANIEL ALVES DA ROCHA <101830720+danizelrocha@users.noreply.github.com>
2022-11-24 17:14:46 -0300

    componentes e rotas

--- rotas/src/app/app.module.ts
+++ rotas/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CursoNaoEncontradoComponent } from './curso-nao-encontrado/curso-nao-encontrado.component';
+import { CursoComponent } from './curso/curso.component';
+import { CursoDetalhesComponent } from './curso-detalhes/curso-detalhes.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CursoNaoEncontradoComponent,
+    CursoComponent,
+    CursoDetalhesComponent,
+    HomeComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
